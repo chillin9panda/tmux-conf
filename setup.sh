f5cfc3ca4b684a38e5e99971a3b31ed9bd4024da
Duressa M <duressamarkos9@gmail.com>
2025-06-21 13:36:38 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 mv ~/.tmux.conf ~/.tmux.conf.bak
 cp .tmux.conf ~/
 
+tmux source ~/.tmux.conf
 ~/.tmux/plugins/tpm/scripts/install_plugins.sh
